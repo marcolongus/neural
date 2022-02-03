1b82a14e8a89dea4aa4473e2f579856d78bdc39b
marcolongus <benjaminmarcolongo@gmail.com>
2022-02-03 19:01:56 -0300

    feedfoward and torch descrip

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms, models
-from torchvision.transforms import ToTensor, Lambda, Compose
+# Import standard libraries
 import matplotlib.pyplot as plt
+import numpy as np
+
+# Import PyTorch modules
+import torch
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+
+# Import torchvision module to handle image manipulation
+from torchvision import datasets
+from torchvision import transforms
+from torchvision import models
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Lambda
+from torchvision.transforms import Compose
+
 
 def data_loader():
+    """
+    Data is a dict over tuples, object[idx] = (image, target).
+    Image is a (28,28) grayscaled image --> needs to be transformed to an array.
+    Target range from 0 to 9. Indicates the label of the image.
+    """
     training_data = datasets.FashionMNIST(
         root="fashion data", train=True, download=True, transform=None
     )
--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import torch
 
-
-
 # Check torch working well
 try:
-	x = torch.rand(1, 1)
-	print("-->",x)
+    x = torch.rand(1, 1)
+    print("-->", x)
 except:
-	print("-->Torch.rand() not working")
+    print("-->Torch.rand() not working")
 finally:
-	print(f"--> CUDA is available: {torch.cuda.is_available()}")
-
+    print(f"--> CUDA is available: {torch.cuda.is_available()}")
