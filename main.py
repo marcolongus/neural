4266827e1e885d26db48026d42fb2090bcc5c4d0
marcolongus <benjaminmarcolongo@gmail.com>
2022-02-02 21:55:22 -0300

    data loader

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import torch
-import numpy as np
-
-
-def fashion_loader():
-    return 0
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
 
 
 # CHECK
 print(f"CUDA is available: {torch.cuda.is_available()}")
-x = torch.rand(5, 3)
+x = torch.rand(1, 1)
 print(x)
+
+
+training_data = datasets.MNIST(root="data", train=True, download=True, transform=None)
+
+print(len(training_data))
