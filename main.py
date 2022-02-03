2453a6d364632bc8ce909860583164c22a79b0e5
marcolongus <benjaminmarcolongo@gmail.com>
2022-02-02 22:04:26 -0300

    fashion data added

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ x = torch.rand(1, 1)
 print(x)
 
 
-training_data = datasets.MNIST(root="data", train=True, download=True, transform=None)
+def data_loader():
+    training_data = datasets.FashionMNIST(
+        root="fashion data", train=True, download=True, transform=None
+    )
+    return training_data
+
+
+training_data = data_loader()
 
 print(len(training_data))
+print(type(training_data))
+
+feature = training_data[8][0]
+label = training_data[8][1]
+
+print(label)
+print(feature.size)
